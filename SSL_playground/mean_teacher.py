b279f2424c4b0cfda061d820eb73ccd0d42cab58
AVanyan <38283090+AVanyan@users.noreply.github.com>
2020-06-30 12:47:00 +0400

    monitor hsic

--- SSL_playground/mean_teacher.py
+++ SSL_playground/mean_teacher.py
@@ -23,21 +23,27 @@ class MeanTeacher(UDA):
 
         sup_batch, unsup_batch = batch_list
 
-        # x, y = sup_batch
-        # y_hat = self.forward(x)
+        x, y = sup_batch
+        y_hat = self.student.forward(x)
 
+        # sup_loss = self.classification_loss(y_hat, y)
+        sup_loss = nll_loss(log_softmax(y_hat, dim=-1), y)
 
         unlabeled, augmented = unsup_batch
         unlab_x, unlab_y = unlabeled
         aug_x, aug_vec = augmented
 
-        unlab_pred_student = self.student.forward(unlab_x)
+        unlab_pred_student = self.student.forward(aug_x)
 
-        sup_loss = nll_loss(log_softmax(unlab_pred_student, dim=1), unlab_y)
+        # sup_loss = nll_loss(log_softmax(unlab_pred_student, dim=1), unlab_y)
 
         with torch.no_grad():
-            unlab_pred_teacher = self.net.forward(unlab_x)
+            unlab_pred_teacher = self.net.forward(aug_x)
 
+        z = unlab_pred_teacher
+        c = aug_vec.type(dtype=torch.cuda.FloatTensor)
+
+        hsic_unsup = hsic.HSIC(z, c)
 
         # z = torch.cat((unlab_pred, augment_pred))
         # c = torch.cat((torch.zeros_like(aug_vec), aug_vec)).type(dtype=torch.cuda.FloatTensor)
@@ -53,7 +59,7 @@ class MeanTeacher(UDA):
         # else:
         #     self.lam = self.max_lam
 
-        unsup_loss = mse_loss(log_softmax(unlab_pred_student, dim=1), log_softmax(unlab_pred_teacher, dim=1))
+        unsup_loss = mse_loss(torch.softmax(unlab_pred_student, dim=-1), torch.softmax(unlab_pred_teacher, dim=-1))
         self.loss = sup_loss + self.lam * unsup_loss
 
         alpha = 0.95
@@ -65,7 +71,7 @@ class MeanTeacher(UDA):
             'train_unsup_acc': self.compute_accuracy(torch.argmax(unlab_pred_teacher, dim=-1), unlab_pred_student),
             'origin_label_logit_acc': self.compute_accuracy(unlab_y, unlab_pred_teacher),
             'training_sup_loss': sup_loss,
-            # 'hsic': hsic_unsup,
+            'hsic': hsic_unsup,
             'unsup_loss': unsup_loss,
             'lambda': self.lam,
             'training_loss': self.loss,
