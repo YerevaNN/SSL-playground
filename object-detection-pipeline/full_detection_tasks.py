6624c3266c67fc17186275af1d341502607aa55f
Hrant Khachatrian <hrant@yerevann.com>
2021-07-21 22:27:04 +0400

    update the pipeline

--- object-detection-pipeline/full_detection_tasks.py
+++ object-detection-pipeline/full_detection_tasks.py
@@ -8,18 +8,29 @@ headers = {'user_secret': TEAM_SECRET, "govteam_secret":GOV_SECRET}
 url = os.environ['LWLL_TA1_API_ENDPOINT']
 r = requests.get(f"{url}/list_tasks", headers=headers)
 tasks = r.json()['tasks']
-image_classification_task = []
+od_tasks = []
 
 for task in tasks:
     r = requests.get(f"{url}/task_metadata/{task}", headers=headers)
-    problem_type = r.json()['task_metadata']['problem_type']
+    r = r.json()['task_metadata']
+    problem_type = r['problem_type']
     if problem_type == 'object_detection':
-        image_classification_task.append(task)
+        od_tasks.append(task)
+        print(",".join([
+            r['task_id'],
+            r['base_dataset'],
+            '/'.join([str(x) for x in r['base_label_budget_full']]),
+            r['adaptation_dataset'],
+            '/'.join([str(x) for x in r['adaptation_label_budget_full']]),
+            ' / '.join(r['whitelist'])
+        ]))
 
-for task in image_classification_task:
+for task in od_tasks:
     try:
         print('================ Starting Task: {} ================'.format(task))
-        os.system('python run_pipeline.py --task={} --session=coral-obj-det-{}'.format(task, task[:5]))
+        cmd = 'python run_pipeline.py --task={} --session=coral-obj-det-{}'.format(task, task[:5])
+        # os.system(cmd)
+        print(cmd)
         print('================ Task: {} Has Finished ================'.format(task))
         print()
     except Exception as err:
