eaac58d9f9ef82faf4eec9cf81c093b85a2b32fe
AVanyan <38283090+AVanyan@users.noreply.github.com>
2021-10-02 14:58:56 +0400

    fix

--- consistency_learning_utils/lightning_model.py
+++ consistency_learning_utils/lightning_model.py
@@ -605,11 +605,9 @@ class STAC(pl.LightningModule):
                 f.write('global_step {} unlab_path {} aug_path {}\n'.format(self.global_step, unlab[2], augment[2]))
         pattern = "last"
         for path, subdirs, files in os.walk(self.save_dir_name_teacher):
-            for name in files:
-                if fnmatch(name, pattern):
-                    print(name)
-                    checkpoint_path = os.path.join(path, name)
-
+            for file in files:
+                if 'last' in file and file != 'last.ckpt':
+                    print(file)
         self.teacher.eval()
         unlab_pred = self.teacher_forward(unlabeled_x, unlabeled_image_paths)
         # save_image(unlabeled_x[0], 'unlabeled.png')
