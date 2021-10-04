a219b972a0fa8e9d4efdf887c9f985d945cebb3a
AVanyan <38283090+AVanyan@users.noreply.github.com>
2021-10-04 18:12:23 +0400

    custom ddp plugin, no sync grads

--- consistency_learning_utils/lightning_model.py
+++ consistency_learning_utils/lightning_model.py
@@ -35,6 +35,9 @@ from pytorch_lightning.plugins import Plugin
 from pytorch_lightning.accelerators import Accelerator
 from pathlib import Path
 
+from pytorch_lightning.plugins.training_type.ddp import DDPPlugin
+from pytorch_lightning.accelerators import GPUAccelerator
+
 from ensemble_boxes import *
 
 def make_target_from_y(y):
@@ -79,87 +82,10 @@ class SkipConnection(nn.Module):
         z = self.module(x)
         return torch.cat((x, z), dim=-1)
 
-class NoSyncTrainer(Trainer):
-    def __init__(self,
-        logger: Union[LightningLoggerBase, Iterable[LightningLoggerBase], bool] = True,
-        checkpoint_callback: bool = True,
-        callbacks: Optional[Union[List[Callback], Callback]] = None,
-        default_root_dir: Optional[str] = None,
-        gradient_clip_val: float = 0,
-        process_position: int = 0,
-        num_nodes: int = 1,
-        num_processes: int = 1,
-        gpus: Optional[Union[List[int], str, int]] = None,
-        auto_select_gpus: bool = False,
-        tpu_cores: Optional[Union[List[int], str, int]] = None,
-        log_gpu_memory: Optional[str] = None,
-        progress_bar_refresh_rate: Optional[int] = None,
-        overfit_batches: Union[int, float] = 0.0,
-        track_grad_norm: Union[int, float, str] = -1,
-        check_val_every_n_epoch: int = 1,
-        fast_dev_run: Union[int, bool] = False,
-        accumulate_grad_batches: Union[int, Dict[int, int], List[list]] = 1,
-        max_epochs: Optional[int] = None,
-        min_epochs: Optional[int] = None,
-        max_steps: Optional[int] = None,
-        min_steps: Optional[int] = None,
-        limit_train_batches: Union[int, float] = 1.0,
-        limit_val_batches: Union[int, float] = 1.0,
-        limit_test_batches: Union[int, float] = 1.0,
-        limit_predict_batches: Union[int, float] = 1.0,
-        val_check_interval: Union[int, float] = 1.0,
-        flush_logs_every_n_steps: int = 100,
-        log_every_n_steps: int = 50,
-        accelerator: Optional[Union[str, Accelerator]] = None,
-        sync_batchnorm: bool = False,
-        precision: int = 32,
-        weights_summary: Optional[str] = 'top',
-        weights_save_path: Optional[str] = None,
-        num_sanity_val_steps: int = 2,
-        truncated_bptt_steps: Optional[int] = None,
-        resume_from_checkpoint: Optional[Union[Path, str]] = None,
-        profiler: Optional[Union[BaseProfiler, bool, str]] = None,
-        benchmark: bool = False,
-        deterministic: bool = False,
-        reload_dataloaders_every_epoch: bool = False,
-        auto_lr_find: Union[bool, str] = False,
-        replace_sampler_ddp: bool = True,
-        terminate_on_nan: bool = False,
-        auto_scale_batch_size: Union[str, bool] = False,
-        prepare_data_per_node: bool = True,
-        plugins: Optional[Union[Plugin, str, list]] = None,
-        amp_backend: str = 'native',
-        amp_level: str = 'O2',
-        distributed_backend: Optional[str] = None,
-        automatic_optimization: Optional[bool] = None,
-        move_metrics_to_cpu: bool = False,
-        enable_pl_optimizer: bool = None,  # todo: remove in v1.3
-        multiple_trainloader_mode: str = 'max_size_cycle',
-        stochastic_weight_avg: bool = False
-    ):
-        super().__init__(logger, checkpoint_callback, callbacks, default_root_dir, gradient_clip_val, process_position,
-                         num_nodes, num_processes, gpus, auto_select_gpus, tpu_cores, log_gpu_memory, progress_bar_refresh_rate,
-                         overfit_batches, track_grad_norm, check_val_every_n_epoch, fast_dev_run, accumulate_grad_batches,
-                         max_epochs, min_epochs, max_steps, min_steps, limit_train_batches, limit_val_batches,
-                         limit_test_batches, limit_predict_batches, val_check_interval, flush_logs_every_n_steps,
-                         log_every_n_steps, accelerator, sync_batchnorm, precision, weights_summary, weights_save_path,
-                         num_sanity_val_steps, truncated_bptt_steps, resume_from_checkpoint, profiler, benchmark,
-                         deterministic, reload_dataloaders_every_epoch, auto_lr_find, replace_sampler_ddp, terminate_on_nan,
-                         auto_scale_batch_size, prepare_data_per_node, plugins, amp_backend, amp_level, distributed_backend,
-                         automatic_optimization, move_metrics_to_cpu, enable_pl_optimizer, multiple_trainloader_mode,
-                         stochastic_weight_avg)
-        self.train_loop = NoSyncTrainLoop(self, multiple_trainloader_mode)
-
-class NoSyncTrainLoop(TrainLoop):
-    def __init__(self, trainer, multiple_trainloader_mode):
-        super().__init__(trainer, multiple_trainloader_mode)
-
-    def should_accumulate(self):
-        # checks if backward or backward + optimizer step (via closure)
-        # accumulation_done = self._accumulated_batches_reached()
-        # is_final_batch = self._num_training_batches_reached()
-        # return not (accumulation_done or is_final_batch)
-        return False
+class NoGradSyncDDP(DDPPlugin):
+    def all_gather(self, tensor: torch.Tensor, group: Optional[Any] = None, sync_grads: bool = False) -> torch.Tensor:
+        """Perform a all_gather on all processes """
+        pass
 
 def model_changed_classifier(reuse_classifier=False, initialize=False, class_num=20, gamma=1, box_score_thresh=0.05):
     """
@@ -301,6 +227,9 @@ class STAC(pl.LightningModule):
             experiment=self.hparams['version_name']
         )
 
+        self.accelerator = GPUAccelerator(
+            training_type_plugin=NoGradSyncDDP(),
+        )
         self.make_teacher_trainer()
         self.make_student_trainer()
 
@@ -453,7 +382,7 @@ class STAC(pl.LightningModule):
         )
         self.teacher_trainer = Trainer(
             gpus=-1, checkpoint_callback=True, # what is this?
-            accelerator='ddp',
+            accelerator=self.accelerator,
             callbacks=[self.t_checkpoint_callback],
             num_sanity_val_steps=0,
             logger=self.aim_logger,
@@ -476,7 +405,6 @@ class STAC(pl.LightningModule):
             check_val_every_n_epoch=self.check_val_epochs,
             deterministic=True,
         )
-
 
     def make_student_trainer(self):
         checkpoint_callback = ModelCheckpoint(
