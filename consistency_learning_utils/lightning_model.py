28073cc722bbc09a6d70d2762dcb172a167401d8
AVanyan <38283090+AVanyan@users.noreply.github.com>
2021-10-04 19:33:36 +0400

    test

--- consistency_learning_utils/lightning_model.py
+++ consistency_learning_utils/lightning_model.py
@@ -385,8 +385,8 @@ class STAC(pl.LightningModule):
         )
         self.teacher_trainer = Trainer(
             gpus=-1, checkpoint_callback=True, # what is this?
-            accelerator='ddp',
-            # plugins=NoGradSyncDDP(),
+            # accelerator='ddp',
+            plugins=NoGradSyncDDP(),
             callbacks=[self.t_checkpoint_callback],
             num_sanity_val_steps=0,
             logger=self.aim_logger,
